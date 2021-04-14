65b59eefd4f7f333ab23b8457e3328f6047b5b9a
antoine volozan <volozan.antoine@gmail.com>
2021-04-14 15:10:12 +0200

    ajout authetification googl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatMenuModule} from '@angular/material/menu';
 import { YagaModule } from '@yaga/leaflet-ng2';
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { AngularFireModule } from '@angular/fire';
     MatSelectModule,
     MatMenuModule,
     YagaModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
